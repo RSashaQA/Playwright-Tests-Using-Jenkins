c5f8e2a1bd7269313a0e20d27b7b5950274afd42
RSashaQA <a.romanov.limehd@gmail.com>
2022-09-20 22:09:04 +0300

    reporter

--- playwright.config.js
+++ playwright.config.js
@@ -35,6 +35,7 @@ const config = {
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -106,4 +107,3 @@ const config = {
   // },
 };
 
-module.exports = config;
